8304ab53ec008f5074f8da9c7f90651aa0cb6ff0
noahfreriksen <noah_freriksen@hotmail.com>
2019-06-06 16:01:02 +0200

    Initial commit

---
+++ boot.py
@@ -0,0 +1,34 @@
+import time
+from umqttsimple import MQTTClient
+import ubinascii
+import machine
+import micropython
+import network
+import esp
+esp.osdebug(None)
+import gc
+gc.collect()
+
+ssid = 'noahhotspot'
+password = '1234hoedjevan'
+mqtt_server = '51.83.42.157'
+#EXAMPLE IP ADDRESS
+#mqtt_server = '192.168.1.144'
+client_id = ubinascii.hexlify(machine.unique_id())
+topic_sub = b'watch/message'
+topic_pub = b'watch/ack'
+
+last_message = 0
+message_interval = 5
+counter = 0
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
